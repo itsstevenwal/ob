890409bd3552c77f823b2f6a3524185c6328e4d8
steven <steven@polymarket.com>
2025-12-03 04:22:55 -0500

    fix: order def

--- src/order.rs
+++ src/order.rs
@@ -1,11 +1,16 @@
 use std::{
     fmt::Display,
+    hash::Hash,
     ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign},
 };
 
-pub trait Id: Eq + Display + Default {}
+/// Trait definition for the order identifier.
+/// This definition makes it easy to reference the trait for objects that use it.
+pub(crate) trait Id: Eq + Display + Default + Hash + Clone {}
 
-pub trait Num:
+/// Trait definition for the numeric type used in the orderbook.
+/// This definition makes it easy to reference the trait for objects that use it.
+pub(crate) trait Num:
     Ord
     + Eq
     + Copy
@@ -24,22 +29,41 @@ pub trait Num:
 
 /// Trait defining the interface for orders in the orderbook
 /// This allows for different order implementations while maintaining a common interface
-pub trait OrderInterface<T: Id, N: Num> {
-    /// Returns the unique identifier for this order
+///
+/// T: The type of the order identifier. Needs to be unique.
+/// N: The numeric type used in the orderbook. Needs to be a number.
+pub trait OrderInterface<T, N>
+where
+    T: Eq + Display + Default + Hash + Clone,
+    N: Ord
+        + Eq
+        + Copy
+        + Default
+        + Display
+        + Add<Output = N>
+        + Sub<Output = N>
+        + Mul<Output = N>
+        + Div<Output = N>
+        + AddAssign<N>
+        + SubAssign<N>
+        + MulAssign<N>
+        + DivAssign<N>,
+{
     fn id<'a>(&'a self) -> &'a T;
 
-    /// Returns the side of this order
     fn is_buy(&self) -> bool;
 
-    /// Returns the price of this order
-    fn price(&self) -> &N;
+    fn price(&self) -> N;
 
-    /// Returns the quantity of this order
-    fn quantity(&self) -> &N;
+    /// Returns the original quantity of this order
+    /// Not updated when the order is filled.
+    fn quantity(&self) -> N;
 
-    // Returns the remaining quantity of this order
-    fn remaining(&self) -> &N;
+    /// Returns the remaining quantity of this order
+    /// Updated when the order is filled.
+    fn remaining(&self) -> N;
 
     /// Fills the order with the specified quantity
+    /// Updates the remaining quantity.
     fn fill(&mut self, quantity: N);
 }
